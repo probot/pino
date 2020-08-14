ff3c90d993065c7eee9c9c11230af2958a1077a2
Gregor Martynus <gregor@martynus.net>
2020-08-14 03:13:33 -0700

    docs(example): add probot-specific log

--- example.js
+++ example.js
@@ -4,6 +4,7 @@ var pino = require("pino")({
   name: "probot",
 });
 
+// random logs
 pino.info("hello world");
 pino.error("this is at error level");
 pino.info("the answer is %d", 42);
@@ -33,3 +34,25 @@ pino
 pino.trace("this is a trace statement");
 
 pino.debug('this is a "debug" statement with "');
+
+// probot-specific logs
+const error = new Error("Oops");
+error.status = 500;
+error.event = {
+  event: "installation_repositories.added",
+  id: "123",
+  installation: 456,
+};
+error.headers = {
+  "x-github-request-id": "789",
+};
+error.request = {
+  headers: {
+    accept: "application/vnd.github.v3+json",
+    authorization: "[Filtered]",
+    "user-agent": "probot/10.0.0",
+  },
+  method: "GET",
+  url: "https://api.github.com/repos/octocat/hello-world/",
+};
+pino.error(error);
