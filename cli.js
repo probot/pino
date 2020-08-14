43a03ce2aafb1412a0fe36991f4c371ec1ff46a2
Gregor Martynus <gregor@martynus.net>
2020-08-14 01:15:10 -0700

    feat: set `LOG_FORMAT=json` and `LOG_LEVEL_IN_STRING=true` in order to get string names in levels (info, error, etc) instead of numbers (30, 50, etc)

--- cli.js
+++ cli.js
@@ -5,6 +5,7 @@ const split = require("split2");
 const prettyFactory = require("pino-pretty");
 
 const formattingEnabled = process.env.LOG_FORMAT !== "json";
+const levelAsString = process.env.LOG_LEVEL_IN_STRING === "true";
 
 const pretty = prettyFactory({
   ignore: [
@@ -22,14 +23,32 @@ const pretty = prettyFactory({
 const probotTransport = new Transform({
   objectMode: true,
   transform(chunk, enc, cb) {
-    const line = pretty(chunk.toString());
+    const line = formattingEnabled
+      ? pretty(chunk.toString())
+      : levelAsString
+      ? stringifyLogLevel(chunk) + "\n"
+      : chunk.toString() + "\n";
+
+    /* istanbul ignore if */
     if (line === undefined) return cb();
+
     cb(null, line);
   },
 });
 
-if (formattingEnabled) {
-  pump(process.stdin, split(), probotTransport, process.stdout);
-} else {
-  pump(process.stdin, process.stdout);
+pump(process.stdin, split(), probotTransport, process.stdout);
+
+const LEVEL_MAP = {
+  "10": "trace",
+  "20": "debug",
+  "30": "info",
+  "40": "warn",
+  "50": "error",
+  "60": "fatal",
+};
+
+function stringifyLogLevel(chunk) {
+  const data = JSON.parse(chunk.toString());
+  data.level = LEVEL_MAP[data.level];
+  return JSON.stringify(data);
 }
