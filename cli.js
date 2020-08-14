6d43bf1f11caa83ecf928aab7c7ee85f8f8796de
Gregor Martynus <gregor@martynus.net>
2020-08-14 03:13:59 -0700

    feat: set `SENTRY_DSN` to send errors to Sentry

--- cli.js
+++ cli.js
@@ -1,11 +1,19 @@
 const { Transform } = require("readable-stream");
 
-const pump = require("pump");
-const split = require("split2");
 const prettyFactory = require("pino-pretty");
+const pump = require("pump");
+const Sentry = require("@sentry/node");
+const split = require("split2");
 
 const formattingEnabled = process.env.LOG_FORMAT !== "json";
 const levelAsString = process.env.LOG_LEVEL_IN_STRING === "true";
+const sentryEnabled = !!process.env.SENTRY_DSN;
+
+if (sentryEnabled) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+  });
+}
 
 const pretty = prettyFactory({
   ignore: [
@@ -23,16 +31,38 @@ const pretty = prettyFactory({
 const probotTransport = new Transform({
   objectMode: true,
   transform(chunk, enc, cb) {
-    const line = formattingEnabled
-      ? pretty(chunk.toString())
-      : levelAsString
-      ? stringifyLogLevel(chunk) + "\n"
-      : chunk.toString() + "\n";
+    const line = chunk.toString();
 
     /* istanbul ignore if */
     if (line === undefined) return cb();
 
-    cb(null, line);
+    const data = sentryEnabled ? JSON.parse(line) : null;
+
+    if (sentryEnabled && data.level >= 50) {
+      Sentry.withScope(function (scope) {
+        const sentryLevelName =
+          data.level === 50 ? Sentry.Severity.Error : Sentry.Severity.Fatal;
+        scope.setLevel(sentryLevelName);
+
+        for (const extra of ["event", "headers", "request", "status"]) {
+          if (!data[extra]) continue;
+
+          scope.setExtra(extra, data[extra]);
+        }
+
+        Sentry.captureException(toSentryError(data));
+      });
+    }
+
+    if (formattingEnabled) {
+      return cb(null, pretty(data || line));
+    }
+
+    if (levelAsString) {
+      return cb(null, stringifyLogLevel(data || JSON.parse(line)));
+    }
+
+    cb(null, chunk.toString() + "\n");
   },
 });
 
@@ -47,8 +77,14 @@ const LEVEL_MAP = {
   "60": "fatal",
 };
 
-function stringifyLogLevel(chunk) {
-  const data = JSON.parse(chunk.toString());
+function stringifyLogLevel(data) {
   data.level = LEVEL_MAP[data.level];
-  return JSON.stringify(data);
+  return JSON.stringify(data) + "\n";
+}
+
+function toSentryError(data) {
+  const error = new Error(data.msg);
+  error.name = data.type;
+  error.stack = data.stack;
+  return error;
 }
