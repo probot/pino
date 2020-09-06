ed4e007c2286bf175b3f095e8b7efe3b57b1bfaf
Gregor Martynus <gregor@martynus.net>
2020-09-06 12:43:35 -0700

    test: a single `\n` is added when using transform function API

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,43 @@
+const Stream = require("stream");
+
 const test = require("tap").test;
+const pino = require("pino");
+const { getTransformStream } = require("..");
 
 test("API", (t) => {
-  t.test("getTransformStream", (t) => {
-    const { getTransformStream } = require("..");
+  t.test("getTransformStream export", (t) => {
     t.isA(getTransformStream, Function);
     t.end();
   });
+
+  t.test(
+    "ONly a single \\n is added to the end log lines when LOG_FORMAT is set to 'json' (https://github.com/probot/probot/issues/1334)",
+    (t) => {
+      process.env.LOG_FORMAT = "json";
+
+      const streamLogsToOutput = new Stream.Writable({ objectMode: true });
+      const output = [];
+      streamLogsToOutput._write = (line, encoding, done) => {
+        output.push(line);
+        done();
+      };
+
+      const transform = getTransformStream();
+      transform.pipe(streamLogsToOutput);
+      const log = pino({}, transform);
+
+      log.info("test");
+
+      t.equal(
+        output.join(""),
+        output.join("").trim() + "\n",
+        'No "\\n" is added to end of line'
+      );
+
+      delete process.env.LOG_FORMAT;
+      t.end();
+    }
+  );
 
   t.end();
 });
