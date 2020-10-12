71d4be4967329afeddac6c73b9454353eab71bf1
Michal Cichra <mikz@users.noreply.github.com>
2020-10-12 18:38:03 +0200

    fix: Fix a crash when there is no "account" property (#30)
    Extract either account the GitHub App is installed to, organization or account as a username.

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,5 @@
+import * as Sentry from "@sentry/node";
+
 const Stream = require("stream");
 
 const test = require("tap").test;
@@ -5,8 +7,87 @@ const pino = require("pino");
 const { getTransformStream } = require("..");
 
 test("API", (t) => {
+  let env = Object.assign({}, process.env);
+
+  t.afterEach((done, t) => {
+    process.env = Object.assign({}, env);
+    done();
+  });
+
   t.test("getTransformStream export", (t) => {
     t.isA(getTransformStream, Function);
+    t.end();
+  });
+
+  t.test("Sentry integation enabled", (t) => {
+    process.env.SENTRY_DSN = "http://username@example.com/1234";
+    const transform = getTransformStream();
+    const log = pino({}, transform);
+
+    function event(payload) {
+      const error = new Error("Hello from the test");
+      error.level = 50;
+      error.event = {
+        payload: Object.assign(
+          {
+            installation: {
+              id: "456",
+            },
+          },
+          payload
+        ),
+      };
+      return error;
+    }
+
+    t.test("without user", (t) => {
+      t.plan(1);
+
+      Sentry.withScope(function (scope) {
+        scope.addEventProcessor(function (event, hint) {
+          t.strictDeepEquals(event.user, { id: "456", username: undefined });
+        });
+
+        log.fatal(event({}));
+      });
+    });
+
+    t.test("with organization", (t) => {
+      t.plan(1);
+
+      Sentry.withScope(function (scope) {
+        scope.addEventProcessor(function (event, hint) {
+          t.match(event.user, { username: "org" });
+        });
+
+        log.fatal(event({ organization: { login: "org" } }));
+      });
+    });
+
+    t.test("with installation account", (t) => {
+      t.plan(1);
+
+      Sentry.withScope(function (scope) {
+        scope.addEventProcessor(function (event, hint) {
+          t.match(event.user, { username: "account" });
+        });
+
+        log.fatal(event({ installation: { account: { login: "account" } } }));
+      });
+    });
+
+    t.test("with repository owner", (t) => {
+      t.plan(1);
+
+      Sentry.withScope(function (scope) {
+        scope.addEventProcessor(function (event, hint) {
+          t.match(event.user, { username: "owner" });
+        });
+
+        log.fatal(event({ repository: { owner: { login: "owner" } } }));
+      });
+    });
+
     t.end();
   });
 
@@ -34,7 +115,6 @@ test("API", (t) => {
         'No "\\n" is added to end of line'
       );
 
-      delete process.env.LOG_FORMAT;
       t.end();
     }
   );
